6fb6decf9302a6ccf4c6004e0e036fb089d9da08
brittnc <84823393+brittnc@users.noreply.github.com>
2021-08-21 23:10:49 -0400

    modified js by adding numbers, adding beginning prompt, and comments

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var generateBtn = document.querySelector("#generate");
 var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var specialCase = ['!','@','#','$','%','&','*','?','/','+','='];
+var numberCase = ['1','2','3','4','5','6','7','8','9'];
 var userChoiceArray=[];
 
 //To begin prompt
@@ -13,14 +14,16 @@ function writePassword() {
     var passwordText = document.querySelector("#password");
 //How many chars
     var userChoice = window.prompt("How many charaters? Choose between 8-128.")
-//Error message
+//Error message if not 8-128 chars
     if(userChoice <8 || userChoice > 128){
          window.prompt("Password must be between 8-128 characters! Please choose between 8-128.");
         };
-//How lower,upper,special chars
+//Would you like lower,upper,special chars?
     var lowerCaseChoice = confirm("Would you like lower case?")
     var upperCaseChoice = confirm("Would you like upper case?")
     var specialCaseChoice = confirm("Would you like special charaters?")
+    var numberCaseChoice = confirm("Would you like numbers?")
+ //If statements   
     if(userChoice){
         console.log(userChoice)
     }   else if(!userChoice){
@@ -37,6 +40,9 @@ function writePassword() {
     if(specialCaseChoice === true){
         userChoiceArray = userChoiceArray.concat(specialCase)
     }
+    if(numberCaseChoice === true){
+        userChoiceArray = userChoiceArray.concat(numberCase)
+    }
 //Randomizer 
     var password = []
     for(var i = 0; i < userChoice; i++){
@@ -44,11 +50,11 @@ function writePassword() {
         var randomCharacter = userChoiceArray[randomIndex]
         password.push(randomCharacter)
     }
-
+//Removes the commas from the generated password
     passwordText.value = password.join("");
     console.log(userChoiceArray)
 };
 
 
-// Add event listener to generate button
+// Event listener
 generateBtn.addEventListener("click", writePassword);
